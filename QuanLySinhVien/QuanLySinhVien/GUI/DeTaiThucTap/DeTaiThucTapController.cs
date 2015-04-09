1c8a11515df3f6fa821b651566d000bbf8f12113
levanhung93 <levanhung.cntt.93@gmail.com>
2015-04-09 15:33:13 +0700

    file de tai thuc tap

---
+++ QuanLySinhVien/QuanLySinhVien/GUI/DeTaiThucTap/DeTaiThucTapController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SQLite;
+using QuanLySinhVien.Common;
+
+namespace QuanLySinhVien.GUI
+{
+    class DeTaiThucTapController
+    {
+        /// <summary>
+        /// hàm thêm thông tin đề tài vào csdl
+        /// </summary>
+        /// <param name="detaiAttribute"></param>
+        public void InsertDeTai(DeTaiThucTapAttribute detaiAttribute)
+        {
+            string query = "INSERT INTO DETAI VALUES ('" + detaiAttribute.MaDeTai + "',N'" + detaiAttribute.TenDeTai + "','" + detaiAttribute.MaGiaoVienHD + "')";
+            DatabaseCore.ExecuteQuery(query);
+        }
+    }
+}
