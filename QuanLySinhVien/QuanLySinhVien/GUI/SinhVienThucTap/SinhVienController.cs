0a2f820b5dd01ae9b1d8cf3f517b32e2dd58483f
levanhung93 <levanhung.cntt.93@gmail.com>
2015-04-09 15:34:31 +0700

    file sinh vien thuc tap

---
+++ QuanLySinhVien/QuanLySinhVien/GUI/SinhVienThucTap/SinhVienController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SQLite;
+using QuanLySinhVien.Common;
+
+namespace QuanLySinhVien.GUI
+{
+    class SinhVienController
+    {
+        /// <summary>
+        /// hàm load mã sinh viên lên _cboMaSinhVien
+        /// </summary>
+        /// <param name="svAttribute"></param>
+        /// <returns></returns>
+        public DataTable LoadMaSinhVien(SinhVienAttribute svAttribute)
+        {
+            string query = "SELECT MaSV FROM SINHVIEN";
+            return DatabaseCore.GetDatatable(query);
+        }
+        /// <summary>
+        /// Hàm lấy toàn bộ tin liên quan đến sinh viên khi đã được chọn mã sinh viên từ _cboMaSinhVien
+        /// </summary>
+        /// <param name="svAttribute"></param>
+        /// <returns></returns>
+        public DataTable GetFullSinhVien(SinhVienAttribute svAttribute)
+        {
+            string query = "SELECT MaSV,TenSV,Ngay,Thang,Nam,GioiTinh,DiaChi,DienThoai,Email,TenLop,KhoaHoc FROM SINHVIEN,LOP,KHOAHOC WHERE MaSV='" + svAttribute.MaSV + "' AND SINHVIEN.MaLop=LOP.MaLop AND LOP.MaKhoa=KHOAHOC.MaKhoa";
+            return DatabaseCore.GetDatatable(query);
+        }
+
+        /// <summary>
+        /// hàm thực hiện thêm sinh viên vào bảng SINHVIENTHUCTAP
+        /// </summary>
+        /// <param name="svAttribute"></param>
+        public void InsertSinhVien(SinhVienAttribute svAttribute)
+        {
+            string query = "INSERT INTO SINHVIENTHUCTAP VALUES ('" + svAttribute.MaSV + "','" + svAttribute.TenSV + "','" + svAttribute.NgaySinh + "', '" + svAttribute.ThangSinh + "','" + svAttribute.NamSinh + "','" + svAttribute.DiaChi + "','" + svAttribute.DienThoai + "','" + svAttribute.GioiTinh + "','" + svAttribute.Email + "','" + svAttribute.Lop + "') ";
+            DatabaseCore.ExecuteQuery(query);
+        }
+
+        /// <summary>
+        /// hàm thực hiện thêm tài khoản. mật khẩu. quyền cho sinh viên
+        /// </summary>
+        /// <param name="svAttribute"></param>
+        public void InsertTaiKhoan(SinhVienAttribute svAttribute)
+        {
+            string query = "INSERT INTO TAIKHOAN VALUES ('" + svAttribute.MaSV + "','" + svAttribute.MaSV + "','SV')";
+            DatabaseCore.ExecuteQuery(query);
+        }
+
+        /// <summary>
+        /// hàm hiển thị thông tin sinh viên thực tập lên _dgvSinhVien
+        /// </summary>
+        /// <returns></returns>
+       public DataTable ShowSinhVien()
+        {
+            String query = "SELECT * FROM SINHVIENTHUCTAP";
+            return DatabaseCore.GetDatatable(query);
+        }
+
+
+    }
+}
